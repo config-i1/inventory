49ebf77f04a4839cb83b91a16d0761c831792a04
Ivan Svetunkov <ivan@svetunkov.ru>
2017-04-12 12:35:07 +0100

    File with the test code for out()

---
+++ R/test.R
@@ -0,0 +1,22 @@
+obs = 1000
+inSample = 100
+holdout = obs - inSample
+
+library(forecast)
+ourData = rnorm(obs, 100, 20)
+
+L = 1
+fcs = array(0, holdout)
+for(i in (inSample+1):(obs)){
+  fcs[i-inSample] = sum(meanf(ourData[1:i], h=L)$mean)
+}
+fcs
+
+outp(data=ourData, fcs=fcs, holdout=holdout, ss="dynamic", L=L, CSL=95)
+outp(data=ourData, fcs=fcs, holdout=holdout, ss="constant", L=L, CSL=95)
+outp(data=ourData, fcs=fcs, holdout=holdout, ss=qnorm(0.5,0,20), L=L, CSL=0.5)
+
+x <- c(6,3,4,4,5,7,4,3,4,5,6,6,7,8,3)
+ftd <- 5
+fcs <- rep(10,8)
+outp(data=x, fcs=fcs, fitted=ftd, holdout=8, ss=3.62845894088858, L=2, CSL=0.95)
